9b6827f66feace5ef7fdd8b9f13aaece6187bb9a
Salman <salmaniqbal00786@gmail.com>
2024-04-09 01:39:05 -0500

    importing project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Nav from "./components/Nav.js";
+import Main from './components/Main.js';
+import Menu from './components/Menu.js';
+import Footer from './components/Footer.js';
+
+
+function App() {
+  return (
+ 
+    <>
+    <Nav/>
+    <Main/>
+    <Menu/>
+    <Footer/>
+    
+    
+    </>
+  );
+}
+
+export default App;
